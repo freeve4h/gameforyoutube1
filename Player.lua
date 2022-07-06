0e368ff3f3f5f6b9826aa1a34e317fe4662d9d48
freeve4h <freeve4h@outlook.com>
2022-07-06 08:10:37 -0400

    yes

---
+++ Player.lua
@@ -0,0 +1,45 @@
+Player = {}
+
+function Player:load()
+    Player.xpos = 400
+    Player.ypos = 300
+    Player.Width = 32
+    Player.Height = 32
+    Player.Speed = 400
+end
+
+function Player:update(dt)
+    Player:checkBounds()
+    if love.keyboard.isDown("up") then 
+        Player.ypos = Player.ypos - Player.Speed * dt
+    end
+    if love.keyboard.isDown("down") then 
+        Player.ypos = Player.ypos + Player.Speed * dt
+    end 
+    if love.keyboard.isDown("right") then 
+        Player.xpos = Player.xpos + Player.Speed * dt
+    end
+    if love.keyboard.isDown("left") then 
+        Player.xpos = Player.xpos - Player.Speed * dt
+    end
+    Player:checkBounds()
+end
+
+function Player:draw()
+    love.graphics.rectangle("fill", Player.xpos, Player.ypos, Player.Width, Player.Height)
+end
+
+function Player:checkBounds()
+    if Player.xpos < 0 then 
+        Player.xpos = 0
+    end 
+    if Player.xpos + Player.Width > love.graphics.getWidth() then 
+        Player.xpos = love.graphics.getWidth() - Player.Width
+    end
+    if Player.ypos < 0 then 
+        Player.ypos = 0
+    end 
+    if Player.ypos + Player.Height > love.graphics.getHeight() then 
+        Player.ypos = love.graphics.getHeight() - Player.Height
+    end
+end
--- main.lua
+++ main.lua
@@ -1,10 +1,19 @@
 require("stolencode")
+require("Player")
 
 function love.load()
+    love.window.setMode(800, 600, {resizable = false})
+    love.window.setTitle("i am running out of video ideas")
+    Player:load()
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("escape") then 
+        love.event.quit()
+    end
+    Player:update(dt)
 end
 
 function love.draw()
+    Player:draw()
 end
